a0c721eb266e7f68342a2090045e609c397db430
Jordan D <j.j.jordan@hotmail.co.uk>
2018-04-12 18:18:43 +0100

    Added product info to ProductConstants

--- src/ProductConstants.java
+++ src/ProductConstants.java
@@ -3,4 +3,11 @@
  */
 public class ProductConstants {
 
+    private static final String APPLE_NAME = "apple";
+    private static final double APPLE_PRICE = 60d;
+
+    private static final String ORANGE_NAME = "orange";
+    private static final double ORANGE_PRICE = 25d;
+
+
 }
